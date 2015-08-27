b067344139e84c61dcbd00abc5919b088a68fab2
Federico Spini <federico.spini@gmail.com>
2015-08-27 17:06:40 +0200

    added page subgenerator

---
+++ generators/page/index.js
@@ -0,0 +1,114 @@
+'use strict';
+
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var path = require('path');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+    var destinationPath = this.destinationRoot();
+    var defaultAppName = destinationPath.split(path.sep).pop();
+    var hasHeader = this.config.get('header');
+    var hasFooter = this.config.get('footer');
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the doozie ' + chalk.red('x-project:page') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'name',
+      message: 'What is the ' + chalk.blue('name') + ' of your brand new page?',
+      default: 'test'
+      // validate: ... TO DO
+    }];
+
+    if (hasHeader) {
+      prompts.push({
+        type: 'confirm',
+        name: 'header',
+        message: 'Would you like to add a link to the new page in the ' + chalk.blue('header') + '?',
+        default: true
+      });
+    }
+
+    if (hasFooter) {
+      prompts.push({
+        type: 'confirm',
+        name: 'footer',
+        message: 'Would you like to add a link to the new page in the ' + chalk.blue('footer') + '?',
+        default: true
+      });
+    }
+
+    this.prompt(prompts, function (props) {
+      // to access props later use:
+      //   this.props.page;
+      this.props = props;
+
+      // save props
+      props.pages = this.config.get('pages') || [];
+      props.pages.push('page-' + props.name);
+      this.config.set('pages', props.pages);
+      this.config.save();
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    page: function () {
+      this.conflicter.force = true;
+      var name = this.props.name;
+
+      var fromPath = 'elements/page-x/page-x.html';
+      var toPath = 'elements/page-' + name + '/page-' + name + '.html';
+
+      var bootToPath = 'elements/app-boot/app-boot.html'
+      var bootFromPath = '../../app/templates/' + bootToPath;
+
+      var headerToPath = 'elements/part-header/part-header.html'
+      var headerFromPath = '../../app/templates/' + headerToPath;
+
+      var footerToPath = 'elements/part-footer/part-footer.html'
+      var footerFromPath = '../../app/templates/' + footerToPath;
+
+
+
+      this.fs.copyTpl(
+        this.templatePath(fromPath),
+        this.destinationPath(toPath),
+        this.props
+      );
+
+      this.fs.copyTpl(
+        this.templatePath(bootFromPath),
+        this.destinationPath(bootToPath),
+        this.props
+      );
+
+      if (this.props.header) {
+        this.fs.copyTpl(
+          this.templatePath(headerFromPath),
+          this.destinationPath(headerToPath),
+          this.props
+        );
+      }
+
+      if (this.props.footer) {
+        this.fs.copyTpl(
+          this.templatePath(footerFromPath),
+          this.destinationPath(footerToPath),
+          this.props
+        );
+      }
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
